066c3bbb2fbeaac9c70d3aa716219fde67c1c704
Henrizzle <hltissink@gmail.com>
2026-04-28 23:35:57 +0300

    feat(web): next-intl request config + plugin wired into next.config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 import path from "node:path";
 import { withSentryConfig } from "@sentry/nextjs";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -9,7 +12,7 @@ const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname, "../../"),
 };
 
-export default withSentryConfig(nextConfig, {
+export default withSentryConfig(withNextIntl(nextConfig), {
   silent: true,
   org: process.env.SENTRY_ORG,
   project: "peron-web",
