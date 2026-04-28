0816b39bedca0e239f30b324471fc639a750d5dc
Henrizzle <hltissink@gmail.com>
2026-04-28 23:37:06 +0300

    feat(web): locale middleware (Accept-Language detection + cookie)

---
+++ apps/web/middleware.ts
@@ -0,0 +1,9 @@
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./src/i18n/routing";
+
+export default createMiddleware(routing);
+
+export const config = {
+  // Match all paths except: api, monitoring (Sentry tunnel), _next, files with extensions
+  matcher: ["/((?!api|monitoring|_next|.*\\..*).*)"],
+};
